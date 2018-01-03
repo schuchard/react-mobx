4fac83781de88709b7f1d297cf85517227804b2f
kevin schuchard <schuchard.kevin@gmail.com>
2018-01-03 06:39:21 -0700

    expose stores to window

--- src/index.js
+++ src/index.js
@@ -11,22 +11,22 @@ import './styles.css';
 import UserList from './components/UserList';
 import UserListModel from './models/UserListModel';
 
-const store = new TodoListModel();
+const todoStore = new TodoListModel();
 const userStore = new UserListModel();
 
 render(
   <div>
     <DevTools />
-    <AppStatus store={store} />
-    <TodoList store={store} />
+    <AppStatus store={todoStore} />
+    <TodoList store={todoStore} />
     <UserList store={userStore} />
   </div>,
   document.getElementById('root')
 );
 
-store.addTodo('Get Coffee');
-store.addTodo('Write simpler code');
-store.todos[0].finished = true;
+todoStore.addTodo('Get Coffee');
+todoStore.addTodo('Write simpler code');
+todoStore.todos[0].finished = true;
 
 userStore.addUserToList({ name: 'Jenna', age: 32, city: 'Fort Collins', state: 'CO' });
 userStore.addUserToList({ name: 'Bill', age: 22, city: 'Dallas', state: 'TX' });
@@ -34,4 +34,4 @@ userStore.addUserToList({ name: 'Joe', age: 54, city: 'Los Angeles', state: 'CA'
 userStore.addUserToList({ name: 'Carl', age: 34, city: 'Denver', state: 'CO' });
 
 // playing around in the console
-window.store = store;
+window.store = { todoStore, userStore };
